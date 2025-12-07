ad1078b1662540ffb76e5c9e3483a8da84266bdb
Kirill <kirillperehodcev684@gmail.com>
2025-12-07 20:01:46 +0300

    Laravel создание Блога 4. Ставим фронт админки, устоновка Admin Lte

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', IndexController::class);
 });
+
+Route::group(["namespace" => 'App\Http\Controllers\Admin', 'prefix'=> 'admin'], function () {
+    Route::group(["namespace" => 'Main'], function () {
+        Route::get("/", "IndexController")->name("index");
+    });
+});
+
 Auth::routes();
