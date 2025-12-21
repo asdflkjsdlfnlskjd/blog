996cc7a595397f63372edd77734394352e5e6d99
Kirill <kirillperehodcev684@gmail.com>
2025-12-21 17:27:14 +0300

    Laravel создание Блога 14. Полностью добавляем основу CRUDа для постов (неполная реализация)

--- resources/views/admin/includes/sitebar.blade.php
+++ resources/views/admin/includes/sitebar.blade.php
@@ -2,9 +2,18 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
     <div class="sidebar">
-        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="{{route('admin.post.index')}}" class="nav-link">
+                    <i class="nav-icon far fa-clipboard"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
             <li class="nav-item">
                 <a href="{{route('admin.category.index')}}" class="nav-link">
                     <i class="nav-icon fas fa-th-list"></i>
---
+++ resources/views/admin/post/Show.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.layouts.main')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6 d-flex align-items-center">
+                    <h1 class="m-0 mr-2">{{$post->title}}</h1>
+                    <td><a href="{{route('admin.post.edit', $post->id)}}" class="text-success" ><i class="fas fa-pencil-alt"></i></a><form method="POST" action="{{route('admin.post.delete', $post->id)}}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="border-0 bg-transparent" type="submit" > <i class="fas fa-trash text-danger" role="button" ></i>
+                            </button>
+                        </form></td>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+                <div class="row"></div>
+                <div class="col-6">
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <tbody>
+                            <tr>
+                                <td>{{$post->id}}</td>
+                                <td><a href="#"><i class="far fa-eye"></i></a></td>
+                            </tr>
+                            <tr>
+                                <td>Название</td>
+                                <td>{{$post->title}}</td>
+                            </tr>
+                            </tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
